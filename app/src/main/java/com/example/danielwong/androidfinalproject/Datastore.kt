2bfd7194a98b0606113837ede7cabeabc44aa084
Daniel Wong <danielwong@Daniels-MacBook-Pro.local>
2018-12-01 19:31:51 -0800

    parsing coordinates working, almost done with parsing

--- app/src/main/java/com/example/danielwong/androidfinalproject/Datastore.kt
+++ app/src/main/java/com/example/danielwong/androidfinalproject/Datastore.kt
@@ -2,19 +2,30 @@ package com.example.danielwong.androidfinalproject
 
 import java.util.*
 import android.util.Log
+import org.json.JSONArray
 
 data class Earthquake (
+    val mag: Double? = null,
     val place: String ="",
-    val id: String? = ""
+    val id: String? = "",
+    val time: Double? = null,
+    val title: String? ="",
+    val latitude: Double? = null,
+    val longitude: Double? = null
 )
 
 data class CompleteJson(val features : List<Feature>) {
     fun generateFeatures(): List<Earthquake> {
         for (f in features) {
+            Log.d("lat", f.geometry.coordinates[0].toString())
             val temp = Earthquake(
+                f.properties.mag,
                 f.properties.place,
-                f.id
-
+                f.id,
+                f.properties.time,
+                f.properties.title,
+                f.geometry.coordinates[0],
+                f.geometry.coordinates[1]
 
             )
             TheFeatures.earthquakes.add(temp)
@@ -27,5 +38,6 @@ object TheFeatures {
     val earthquakes : MutableList<Earthquake> = mutableListOf()
 }
 
-data class Feature(val properties: Properties, val id:String?)
-data class Properties(val place: String)
+data class Feature(val properties: Properties, val id:String?, val geometry: Geometry)
+data class Properties(val place: String, val mag: Double, val time: Double?, val title: String)
+data class Geometry(val coordinates: Array<Double>)
--- app/src/main/java/com/example/danielwong/androidfinalproject/MapsActivity.kt
+++ app/src/main/java/com/example/danielwong/androidfinalproject/MapsActivity.kt
@@ -106,7 +106,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 // generateEarthquakes
                 val earthquakes = EarthquakeData.generateFeatures()
                 for (next in earthquakes) {
-                    Log.d("tag", next.id + " " + next.place)
+                    Log.d("tag", next.id + " " + next.place + " " + next.mag)
                 }
             } catch (ex: Exception) {
                 println("JSON parsing exception" + ex.printStackTrace())
