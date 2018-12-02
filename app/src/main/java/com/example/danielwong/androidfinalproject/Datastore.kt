2b9eefadf10fe35bffd6cf046a1dbb020bdbfd96
Daniel Wong <danielwong@Daniels-MacBook-Pro.local>
2018-12-01 17:04:59 -0800

    didn't break. added datastore

--- app/src/main/java/com/example/danielwong/androidfinalproject/Datastore.kt
+++ app/src/main/java/com/example/danielwong/androidfinalproject/Datastore.kt
@@ -7,6 +7,18 @@ data class Feature (
     val id : String? = ""
 )
 
+data class CompleteJson(val features : List<Feature>) {
+    fun generateFeatures(): List<Feature> {
+        for (f in features) {
+            val temp = Feature(
+                f.id
+            )
+            TheFeatures.FEATURES.add(temp)
+        }
+        return TheFeatures.FEATURES
+    }
+}
+
 object TheFeatures {
     val FEATURES : MutableList<Feature> = mutableListOf()
 }
